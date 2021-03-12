f2e285e93b93b32a4166fe45ae3e7356276b568d
xiongyu <xiongyu@espressif.com>
2021-03-12 18:18:13 +0800

    support gdb

--- projects/test.sh
+++ projects/test.sh
@@ -1,9 +1,13 @@
 WORK_PATH=$(cd $(dirname $0); pwd)
+export PATH=$PATH:$RISCV_NONE_EMBED_GCC_PATH
 
 cd ${WORK_PATH}/tinyriscv/tests/example/FreeRTOS/Demo/tinyriscv_GCC
+make clean
 make -j4
 
 cd ${WORK_PATH}/tinyriscv/tests/example/uart_tx
+make clean
 make -j4
 
-telnet
+# telnet
+riscv-none-embed-gdb ${WORK_PATH}/tinyriscv/tests/example/uart_tx/uart_tx -x ${WORK_PATH}/tinyriscv/tests/example/gdbinit
